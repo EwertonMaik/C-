1c177e8958defb964e5c6d0a9c1c90d374f11a45
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-08 10:51:17 -0300

    Update Classe.cs

--- Classe.cs
+++ Classe.cs
@@ -6,7 +6,8 @@ namespace Primeiro // Define que a Classe(Program) abaixo está organizada dentr
     {
         static void Main(string[] args) // Declaração padrão do entry point, ponto de de entrada, inicio do programa
         {
-            Console.WriteLine("Primeiro Programa C#"); //Método do NameSpace System para imprimir valores escritos ou passado por variáveis ou parâmetros
+            Console.WriteLine("Primeiro Programa C#"); //Método do NameSpace System para imprimir valores escritos ou passado por variáveis ou parâmetros.
+            Console.Write("Imprime e não quebra linha"); // Diferente do WriteLine() que imprime e quebra linha, o Write() não quebra a linha impressa.
             
         }//Fim do método MAIN
 
