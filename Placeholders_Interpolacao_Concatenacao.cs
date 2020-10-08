f846a88a4b126c371d4e219c81a5696dbabea7d8
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-08 14:25:27 -0300

    Create Placeholders_Interpolacao_Concatenacao.cs

---
+++ Placeholders_Interpolacao_Concatenacao.cs
@@ -0,0 +1,16 @@
+using System; //NameSpace da Linguagem que contêm diversas classes e métodos
+
+namespace Primeiro // Define que a Classe(Program) abaixo está organizada dentro do namespace(Primeiro) - Agrupa códigos do Projeto 
+{
+    class Program // Definição da Classe
+    {
+        static void Main(string[] args) // Declaração padrão do entry point, ponto de de entrada, inicio do programa
+        {
+            Console.WriteLine("Primeiro Programa C#"); //Método do NameSpace System para imprimir valores escritos ou passado por variáveis ou parâmetros.
+            Console.Write("Imprime e não quebra linha"); // Diferente do WriteLine() que imprime e quebra linha, o Write() não quebra a linha impressa.
+            
+        }//Fim do método MAIN
+
+    }//Fim da Classe Program
+
+}//Fim Namespace
