4f3a01789c5da81b8a016969f87bba3bb8dabf5f
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-09 16:01:20 -0300

    Add files via upload

---
+++ Circulo/CalcCirculo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class CalcCirculo
+    {
+        public static double PI = 3.14159;
+
+        //Método Estático para Cálcular Circunferencia CIRCULO
+        //Formula C = 2 * PI * R ou C = D * PI
+        public static double CircunferenciaC(double r)
+        {
+            return 2.0 * PI * r;
+        }
+
+        //Método Estático para Cálcular Volume
+        public static double VolumeC(double r)
+        {
+            return 4.0 / 3.0 * PI * r * r * r;
+        }
+
+        //Método Estático para Cálcular Área CIRCULO
+        // A = PI * R² OU A = (PI * D²) / 4
+        public static double AreaC(double r)
+        {
+            return PI * (r * r);
+        }
+
+        //Método Estático para Cálcular Diâmetro CIRCULO
+        // D = 2 * R
+        public static double DiametroC(double r)
+        {
+            return 2 * r;
+        }
+
+        //Método Estático para Cálcular o Raio CIRCULO
+        // R = D / 2
+        public static double RaioC(double d)
+        {
+            return d / 2;
+        }
+
+    }
+}
---
+++ Circulo/Circulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Primeiro
+{
+    //Classe Circulo para representar Calculos de Circulo
+    //Usando Membros Estáticos, podem ser chamados pelo próprio nome e não precisa instânciar
+    class Circulo
+    {
+        public static void Main(String[] args)
+        {
+            Console.Write("Entre com o valor Raio para Cálcular Circunferência: ");
+            double raio = double.Parse(Console.ReadLine() );
+
+            Console.WriteLine("Circunferência é : {0}", CalcCirculo. CircunferenciaC(raio) );
+            Console.WriteLine("Volume é: {0}", CalcCirculo.VolumeC(raio) );
+            Console.WriteLine("Área é: {0}", CalcCirculo.AreaC(raio) );
+            Console.WriteLine("Diametro é: {0}", CalcCirculo.DiametroC(raio) );
+
+            Console.WriteLine("Entre com o Diametro de um Circulo: ");
+            double diam = double.Parse(Console.ReadLine() );
+            Console.WriteLine("Raio é: {0}", CalcCirculo.RaioC(diam) );
+        }
+    }
+}
