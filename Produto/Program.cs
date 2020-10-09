50236d91524b54648d9c931969062ed640ff11c9
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-09 19:50:11 -0300

    Update Program.cs

--- Produto/Program.cs
+++ Produto/Program.cs
@@ -7,26 +7,27 @@ namespace Primeiro // Define que a Classe(Program) abaixo está organizada dentr
         static void Main(string[] args) // Declaração padrão do entry point, ponto de de entrada, inicio do programa
         {
             //Programa Classe Produto
-            Produto p = new Produto();
+           Console.Write("Nome :");
+           string Nome = Console.ReadLine();
+           Console.Write("Preço :");
+           double Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
+           Console.Write("Quantidade :");
+           int Quant = int.Parse(Console.ReadLine() );
+           
+           //Classe Produto Instãnciada e valores passados por parãmetro
+           Produto p = new Produto(Nome, Preco, Quant);
 
-            Console.Write("Nome :");
-            p.Nome = Console.ReadLine();
-            Console.Write("Preço :");
-            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
-            Console.Write("Quantidade :");
-            p.Quant = int.Parse(Console.ReadLine() );
+           Console.WriteLine("Dados do Produto: " + p); //A concatenação com o Objeto p irá chamar automaticamente o método ToString
 
-            Console.WriteLine("Dados do Produto: " + p); //A concatenação com o Objeto p irá chamar automaticamente o método ToString
+           Console.Write("Digite o Nº de Produtos a ser adicionado: ");
+           int n_qtd = int.Parse(Console.ReadLine() );
+           p.AdicionarProduto(n_qtd);
+           Console.WriteLine("Dados Atualizado : " + p);
 
-            Console.Write("Digite o Nº de Produtos a ser adicionado: ");
-            int n_qtd = int.Parse(Console.ReadLine() );
-            p.AdicionarProduto(n_qtd);
-            Console.WriteLine("Dados Atualizado : " + p);
-
-            Console.Write("Digite o Nº de Produtos a ser removidos: ");
-            n_qtd = int.Parse(Console.ReadLine() );
-            p.RemoverProduto(n_qtd);
-            Console.WriteLine("Dados Atualizados : " + p);
+           Console.Write("Digite o Nº de Produtos a ser removidos: ");
+           n_qtd = int.Parse(Console.ReadLine() );
+           p.RemoverProduto(n_qtd);
+           Console.WriteLine("Dados Atualizados : " + p);
             
         }//Fim do método MAIN
 
