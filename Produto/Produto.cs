39cdc64868bad3e4597c136cd02f079b0d584bce
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-09 10:01:36 -0300

    Add files via upload

---
+++ Produto/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro
+{
+    //Classe para representar estrutura e métodos de Produto
+    class Produto
+    {
+        public string nome;
+        public double preco;
+        public int    quant;
+
+        //Método Total de Produto
+        public double ValorTotalEstoque()
+        {
+            return preco * quant;
+        }
+
+        //Método Override para ToString
+        public override string ToString()
+        {
+            return nome + ", $ " + preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + quant +
+                " unidades, Total: $ "
+                + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
