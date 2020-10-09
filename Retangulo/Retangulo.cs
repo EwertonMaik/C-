0b4800e25c6757f82c903245649d4b75076e313c
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-09 11:00:32 -0300

    Add files via upload

---
+++ Retangulo/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Primeiro
+{
+    //Classe para representar calculos de Retângulo
+    class Retangulo
+    {
+        public double B; // Base Retângulo
+        public double A; // Altura Retângulo
+
+        //Método Calcula AREA A = B * A
+        public double AreaTriangulo()
+        {
+            return B * A;
+        }
+
+        //Método Calcula PERIMETRO P = 2 * (B + A)
+        public double PerimetroTriangulo()
+        {
+            return 2 * (B + A);
+        }
+
+        //Método Calcula DIAGONAL D = RAIZ² de(B² + A²) OU D² = B² + A²
+        public double DiagonalTriangulo()
+        {
+            return Math.Sqrt(B * B + A * A);
+        }
+    }
+}
