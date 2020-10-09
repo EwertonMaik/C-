337cab86b83f6b39a781f483e2735688cc0abf53
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-09 10:28:17 -0300

    Update Program.cs

--- Produto/Program.cs
+++ Produto/Program.cs
@@ -7,16 +7,26 @@ namespace Primeiro // Define que a Classe(Program) abaixo está organizada dentr
         static void Main(string[] args) // Declaração padrão do entry point, ponto de de entrada, inicio do programa
         {
             //Programa Classe Produto
-            Produto p = new Produto(); //Declarando e Instanciando Objeto Produto
+            Produto p = new Produto();
 
             Console.Write("Nome :");
-            p.nome = Console.ReadLine();
+            p.Nome = Console.ReadLine();
             Console.Write("Preço :");
-            p.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
             Console.Write("Quantidade :");
-            p.quant = int.Parse(Console.ReadLine() );
+            p.Quant = int.Parse(Console.ReadLine() );
 
             Console.WriteLine("Dados do Produto: " + p); //A concatenação com o Objeto p irá chamar automaticamente o método ToString
+
+            Console.Write("Digite o Nº de Produtos a ser adicionado: ");
+            int n_qtd = int.Parse(Console.ReadLine() );
+            p.AdicionarProduto(n_qtd);
+            Console.WriteLine("Dados Atualizado : " + p);
+
+            Console.Write("Digite o Nº de Produtos a ser removidos: ");
+            n_qtd = int.Parse(Console.ReadLine() );
+            p.RemoverProduto(n_qtd);
+            Console.WriteLine("Dados Atualizados : " + p);
             
         }//Fim do método MAIN
 
