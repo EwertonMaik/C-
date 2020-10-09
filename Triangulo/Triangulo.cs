86bb8903d1f2684a7e1d0e031377b270a744a6dc
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-08 21:22:20 -0300

    Add files via upload

---
+++ Triangulo/Triangulo.cs
@@ -0,0 +1,11 @@
+﻿namespace Primeiro
+{
+    //Classe para representar a estrutura de um Triângulo
+    class Triangulo
+    {
+        //Variáveis publicas da Classe
+        public double A;
+        public double B;
+        public double C;
+    }
+}
