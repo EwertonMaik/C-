afb11ce5443ab914ecf0d6385f31e1a39e87d779
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-11 17:45:27 -0300

    Add files via upload

---
+++ BoxingUnboxing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Primeiro
+{
+    //Exemplo de Boxing e Unboxing
+    class BoxingUnboxing
+    {
+        //Método Main
+        static void Main(string[] args)
+        {
+            //Exemplo 01 - BOXING
+            //Processo de Conversão de um objeto tipo VALOR para um tipo REFERÊNCIA
+            int x = 20; // Var tipo valor
+            Object y = x;
+
+            //Exemplo 02 - UNBOXING
+            //Processo de Conversão de um Objeto tipo REFERÊNCIA para um tipo VALOR (Compatível)
+            int a = (int) y; //a (Tipo valor) recebe um CATS (int) de y (Tipo Ref.)
+        }
+    }
+}
