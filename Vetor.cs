f9d6ff88239e3b53b3cfa090e401caf92b9aa647
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-11 12:18:27 -0300

    Add files via upload

---
+++ Vetor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro
+{
+    //Classe para validar vetor
+    class Vetor
+    {
+        //Método Main
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nº de Posições que o vetor terá: ");
+            int n = int.Parse(Console.ReadLine() );
+
+            double[] vetor = new double[n]; //Declarando o vetor e o tamanho que irá ter atraves do parâmetro n
+
+            double soma = 0;
+
+            //Estrutura fora para percorrer e popular o vetor e contar os valores do vetor
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite a {0}ª altura: ", i + 1);
+                vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
+                soma += vetor[i];
+            }
+
+            Console.WriteLine("A soma das alturas é {0}", soma);
+            Console.WriteLine("A média das alturas é {0}", (soma / n).ToString("F2", CultureInfo.InvariantCulture) );
+        }
+    }
+}
