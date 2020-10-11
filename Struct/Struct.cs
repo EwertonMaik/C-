e87e5df86f58528806a75855ea30322b40736cd7
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-11 10:41:11 -0300

    Add files via upload

---
+++ Struct/Struct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Primeiro.Struct
+{
+    //struct tipo de dados valor
+    //struct - Pode ser criado e já atribuido valores ou instânciado como uma classe e atribuido valores
+    //Classes são estruturas por referência e aceita null
+    //Struct pode ser criado com a estrutura abaixo
+    struct Struct
+    {
+        //O tipos de dados primitivos são variavéis tipo valor Ex: int, double, float, byte etc...
+        //Diferente das classes que são tipo referência
+        public double x;
+        public double y;
+
+        //Sobrecarga para o Método ToString
+        public override string ToString()
+        {
+            return "( " + x + ", " + y + ")";
+        }
+
+    }
+}
---
+++ Struct/StructProgam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Primeiro.Struct
+{
+    class StructProgam
+    {
+        //Método Main
+        static void Main(string[] args)
+        {
+            //Declarando uma variável Struct
+            Struct str;
+
+            //Sem instânciar é possivel já atribuir valores para os atributos
+            str.x = 10;
+            str.y = 20;
+            Console.WriteLine(str); //Imprimindo valor
+
+            //Pode ser intânciada também
+            str = new Struct();
+            Console.WriteLine(str); //Porém será impresso 0, pois não foi atribuido nenhum valor
+        }
+    }
+}
