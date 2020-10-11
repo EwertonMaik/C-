485f3f27e1db943477c318a74eb4d84a2aa6aa40
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-11 17:27:54 -0300

    Add files via upload

---
+++ Modificador_Parametro/Ref_Out/ModificadorOut.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Primeiro.Modificador_Parametro.Ref_Out
+{
+    //Classe para utilizar Modificador OUT
+    class ModificadorOut
+    {
+        //Método Main
+        static void Main(string[] args)
+        {
+            //Chamada do Método
+            int a = 10; // Variável declarada e inicializada, será duplicada pelo Método Triple
+            int triple; //Variável apenas declarada, irá receber o valor de (a) triplicado
+            ModificadorOut.Triple(a, out triple); //Passado o valor de (a), e o resultado virá para a variável (triple)
+            Console.WriteLine(triple);
+        }
+
+        //Método para Triplicar Número usando OUT
+        //Comparado ao REF, a variavél OUT não precisa ser inicializada
+        //É uma método void, que não retorna nada, porém o valor calculado dentro deste escopo
+        //Só é passado para outro escopo devido ao Modificador OUT
+        public static void Triple (int valor, out int resultado)
+        {
+            resultado = valor * 3;
+        }
+    }
+}
---
+++ Modificador_Parametro/Ref_Out/ModificadorRef.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Primeiro.Modificador_Parametro.Ref_Out
+{
+    //Classe para utilizar Modificador REF
+    class ModificadorRef
+    {
+        //Método Main
+        static void Main(string[] args)
+        {
+            int a = 10; ; //Variável declarada e atribuido valor para ser triplicado pela função
+            ModificadorRef.Triple(ref a); //Assim como no método, aqui na chamada da função é preciso especificar REF, pois só assim o valor será referênciado
+            Console.WriteLine(a);
+        }
+
+        //Método para Triplicar Número usando REF
+        //É uma método void, que não retorna nada, porém o valor calculado dentro deste escopo
+        //Só é passado para outro escopo devido ao Modificador REF
+        //Se não tivesse REF, esse valor de x não era passado e destruido quando saisse do escopo
+        public static void Triple(ref int x)
+        {
+            x *= 3;
+        }
+    }
+}
