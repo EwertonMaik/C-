d13cdd0b1d8dc877c5a3285d9e0fd603b8c014af
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-11 13:15:22 -0300

    Add files via upload

---
+++ VetorClass/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primeiro
+{
+    //Classe Product para ser usada em um vetor
+    class Product
+    {
+        //Atributos com AutoProperties de get e set
+        public string Nome { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ VetorClass/Vetor_Class.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro
+{
+    //Classe Vetor_Class para usar vetor de um Classe Product
+    class Vetor_Class
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nº de Posições que o vetor terá:");
+            int n = int.Parse(Console.ReadLine() );
+
+            //Declarando e instânciando um vetor tipo Classe
+            Product[] vetproduct = new Product[n]; // Quantidade no valor do parâmetro
+
+            double soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nome Produto: ");
+                string nome = Console.ReadLine();
+                Console.Write("Preço: ");
+                double price = double.Parse(Console.ReadLine() );
+                vetproduct[i] = new Product {Nome = nome, Price = price };
+                /*
+                 A atribuição pode ser da seguinte forma também
+                 vetproduct[i].Price = price;
+                 vetproduct[i].Nome  = nome;
+                 */
+                soma += vetproduct[i].Price;
+                
+            }
+
+            //Imprimindo valores
+            Console.WriteLine("Preço Médio é {0}", (soma / n).ToString("F2", CultureInfo.InvariantCulture) );
+        }
+    }
+}
