2348821b348a0f344d0c4a475e0bf790e696ac06
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-12 22:29:27 -0300

    Add files via upload

---
+++ Matriz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Primeiro
+{
+    //Classe para utilizar Matrizes
+    class Matriz
+    {
+        //Método Main
+        static void Main(string[] args)
+        {
+            //Declarado e Intãnciado uma Matriz Bidimencional
+            double[,] matriz = new double[2,3]; //2 Linhas e 3 Colunas
+
+            //Mostra o tamanho da Matriz
+            Console.WriteLine("Tamanho da Matriz : " + matriz.Length);
+
+            //Mostra a quantidade de Linhas da Matriz
+            Console.WriteLine("Quantidade Linhas Matriz : " + matriz.Rank);
+
+            //Mostra a Dimensão da Matiz 
+            Console.WriteLine("Dimensão da Matriz : " + matriz.GetLength(0) );
+        }
+    }
+}
