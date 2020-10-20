a79c7d6f105b2fd01b26fbe6977f50bd1b816cee
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-20 17:23:40 -0300

    Add files via upload

---
+++ WorkingWithFile/FileInfoAndFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingWithFile
+{
+    class FileInfoAndFile
+    {
+        static void Main(string[] args)
+        {
+            string caminhoArquivo = @"C:\Udemy\C# COMPLETO 2020 Programação Orientada a Objetos + Projetos 37_horas\CAP_13\File1.txt";
+            string caminhoDestino = @"C:\Udemy\C# COMPLETO 2020 Programação Orientada a Objetos + Projetos 37_horas\CAP_13\File2.txt";
+            
+            try
+            {
+                //Usando FileInfo
+                FileInfo fileInfo = new FileInfo(caminhoArquivo); //Instância Objeto FileInfo do caminho do arquivo informado
+                //Das diversas funções fornecidas pela Classe
+                fileInfo.CopyTo(caminhoDestino); //Realiza Copia do arquivo! Para o caminho + nome do aquivo de destino, informado no parãmetro
+                //Na segunda vez que executar o programa, o 2º arquivo irá já existir, e um Exceção será capturada
+                Console.WriteLine("Copia do Arquivo Realizada com Sucesso para o novo Destino!");
+
+                //Usando File para pegar o conteudo e imprimir em tela
+                string[] linhas = File.ReadAllLines(caminhoArquivo); //Lê cada linha do arquivo e salva em um vetor de strings
+                foreach (string obj in linhas)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine("Ocorreu Erro : " + ioe.Message);
+            }
+        
+        }
+    }
+}
---
+++ WorkingWithFile/FileStreamAndStreamReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingWithFile
+{
+    //Classe para testar FileStream e StreamReader
+    class FileStreamAndStreamReader
+    {
+        static void Main(string[] args)
+        {
+            //Declarando Variáveis
+            string caminhoArquivo = @"C:\Udemy\C# COMPLETO 2020 Programação Orientada a Objetos + Projetos 37_horas\CAP_13\File1.txt";
+            FileStream fileStream = null;
+            StreamReader streamReader = null;
+
+            try
+            {
+                fileStream = new FileStream(caminhoArquivo, FileMode.Open); //Intânciado Objeto para apenas abri e o arquivo informado
+                streamReader = new StreamReader(fileStream); //Instância o Objeto StreamReader a partir de FileStream
+                //streamReader = new StreamReader( new FileStream(caminhoArquivo, FileMode.Open)); //Uma segunda forma de Instãnciar o  Objeto StreamReader
+                //streamReader = File.OpenText(caminhoArquivo); //Terceira forma de Instãnciar o arquivo, e neste caso não precisar usar FileStream, apenas StreamReader e File
+                string linha = streamReader.ReadLine();
+                Console.WriteLine("Leu apenas uma Linha - " + linha);
+
+                //Enquanto não chegar no final do arquivo
+                int i = 1;
+                while (! streamReader.EndOfStream) // Método que lê até o final do arquivo
+                {
+                    linha = streamReader.ReadLine();
+                    Console.WriteLine($"Linha - {i} : {linha}");
+                    i++;
+                }
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine("Erro IOException : " + ioe.Message);
+            }
+            finally // Usando o Bloco Finally que sempre é executado para finalizar a conexão com o arquivo!
+            {
+                if (streamReader != null) streamReader.Close();
+                if (fileStream != null) fileStream.Close();
+                Console.WriteLine("Conexão Fechadas!");
+            }
+
+        }
+    }
+}
