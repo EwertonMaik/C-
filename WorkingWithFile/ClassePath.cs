13ad12ee00af8a5c9adcb2f51f8f0e0bbb92aec1
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-20 23:02:57 -0300

    Add files via upload

---
+++ WorkingWithFile/ClassePath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingWithFile
+{
+    //Classe para Testar Funções da Classe Path
+    class ClassePath
+    {
+        static void Main(string[] args)
+        {
+            //Definindo variável para o caminho
+            string varPath = @"C:\Udemy\C# COMPLETO 2020 Programação Orientada a Objetos + Projetos 37_horas\CAP_13\File1.txt";
+
+            //Testando e imprimindo valores sobre o Caminho do Arquivo
+            Console.WriteLine(" DirectorySeparatorChar       : " + Path.DirectorySeparatorChar);
+            Console.WriteLine(" PathSeparator                : " + Path.PathSeparator);
+            Console.WriteLine(" GetDirectoryName             : " + Path.GetDirectoryName(varPath) );
+            Console.WriteLine(" GetFileName                  : " + Path.GetFileName(varPath) );
+            Console.WriteLine(" GetExtension                 : " + Path.GetExtension(varPath) );
+            Console.WriteLine(" GetFileNameWithoutExtension  : " + Path.GetFileNameWithoutExtension(varPath) );
+            Console.WriteLine(" GetFullPath                  : " + Path.GetFullPath(varPath) );
+            Console.WriteLine(" GetTempPath                  : " + Path.GetTempPath() ); // Caminho da pasta tempo do SO
+        }
+    }
+}
