44fdd6633d99deb597c855cdd2363cc7bc34086e
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-22 16:09:50 -0300

    Add files via upload

---
+++ WorkingGenerics/GetEqualsAndGetHashCode/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkingGenerics.GetEqualsAndGetHashCode
+{
+    //Classe Entidade Cliente, para implementar método sobrescrito de equals e hashcode
+    class Cliente
+    {
+        //Atributos AutoProperties
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        //Implementando GetEquals
+        public override bool Equals(object obj)
+        {
+            if (! (obj is Cliente))
+            {
+                throw new ArgumentException("Erro ArgumentException - Não é um Cliente!");
+            }
+
+            Cliente cli = obj as Cliente;
+            return Email.Equals(cli.Email);
+        }
+
+        //Método GetHashCode
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ WorkingGenerics/GetEqualsAndGetHashCode/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WorkingGenerics.GetEqualsAndGetHashCode
+{
+    //Classe para testar GetEquals e GetHashCode
+    class Program
+    {
+        //Método Main
+        static void Main(string[] args)
+        {
+            //Declarando variáveis
+            string a = "Maria";
+            string b = "Carla";
+            string c = "Maria";
+
+            //Testanto GetEquals
+            Console.WriteLine("Equals - Maria é igual Carla : " + a.Equals(b) ); // Resultado FALSE
+            Console.WriteLine("Equals - Maria é igual Maria : " + a.Equals(c) ); // Resultado TRUE
+
+            //Testando GetHashCode
+            Console.WriteLine("GetHashCode - a - Maria : " + a.GetHashCode() );
+            Console.WriteLine("GetHashCode - b - Carla : " + b.GetHashCode() );
+            Console.WriteLine("GetHashCode - c - Maria : " + c.GetHashCode() );
+
+            Console.WriteLine("********************************************************");
+            //**************************************
+            //Testando Classe CLIENTE
+            Cliente cli01 = new Cliente() { Nome = "Maria", Email = "maria@gmail.com" };
+            Cliente cli02 = new Cliente() { Nome = "Carla", Email = "carla@gmail.com"};
+            Cliente cli03 = new Cliente() { Nome = "Maria", Email = "maria@gmail.com" };
+
+            Console.WriteLine("cli01.Equals(cli02) : " + cli01.Equals(cli02) );
+            Console.WriteLine("cli01.Equals(cli03) : " + cli01.Equals(cli03));
+            Console.WriteLine("cli01.GetHashCode() : " + cli01.GetHashCode() );
+            Console.WriteLine("cli02.GetHashCode() : " + cli02.GetHashCode());
+            Console.WriteLine("cli03.GetHashCode() : " + cli03.GetHashCode());
+
+        }
+    }
+}
