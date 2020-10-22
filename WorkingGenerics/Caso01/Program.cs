79aeca58fa674c272e7b53efb1d9df40b229aff1
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-22 12:09:33 -0300

    Add files via upload

---
+++ WorkingGenerics/Caso01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WorkingGenerics.Caso01
+{
+    //Classe Principal
+    class Program
+    {
+        //Método Principal
+        static void Main(string[] args)
+        {
+            //Declarando variável
+            ServicoImpressao simp = new ServicoImpressao();
+
+            Console.Write("Quantos valores deseja digitar? : ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)//Para controlar a quantidade que será digitada
+            {
+                Console.Write($"Valor {i} : ");
+                int x = int.Parse(Console.ReadLine() );
+                simp.AdicionarValores(x);
+            }
+
+            //Imprimindo Valores
+            simp.Imprimir();
+            Console.WriteLine("Primeiro Valor : " + simp.PrimeiroValor() );
+        }
+    }
+}
---
+++ WorkingGenerics/Caso01/ServicoImpressao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace WorkingGenerics.Caso01
+{
+    //Classe entidade contêm os serviços de impressão
+    class ServicoImpressao
+    {
+        //Atributos
+        private int[] _valores = new int[10];
+        private int _count = 0; //Para controlar as posições
+
+        //Método para Adicionar valores ao Vetor
+        public void AdicionarValores(int valor)
+        {
+            if (valor == 10) ///Lança exceção de erro caso esteja cheio
+            {
+                throw new InvalidOperationException("InvalidOperationException - Vetor está cheio!");
+            }
+            _valores[_count] = valor;
+            _count++;
+        }
+
+        //Método para retornar o primeiro elemento do Vetor
+        public int PrimeiroValor()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("InvalidOperationException - Vetor é Vazio ainda!");
+            }
+            return _valores[0];
+        }
+
+        //Método para imprimir valor
+        public void Imprimir()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count -1; i++) //Imprime da Posição 0 a penultima
+            {
+                Console.Write(_valores[i] + ", ");
+            }
+            if (_count > 0) //Imprime a Última posição com valor
+            {
+                Console.Write(_valores[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
