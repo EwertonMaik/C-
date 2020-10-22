a1c2003b6bd7364343a3a8cfee9d9bc9f4b80c0c
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-22 18:04:30 -0300

    Add files via upload

---
+++ WorkingGenerics/HashSetAndSortedSet/HashSetOp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkingGenerics.HashSetAndSortedSet
+{
+    //Classe para Testar HashSet
+    class HashSetOp
+    {
+        //Método Main
+        static void Main (string[] args)
+        {
+            //Declarando uma variável hashSet
+            HashSet<string> listaHashSet = new HashSet<string>();
+
+            //Adicionando valores a Lista manualmente
+            listaHashSet.Add("Bicicleta");
+            listaHashSet.Add("Moto");
+            listaHashSet.Add("Carro");
+            listaHashSet.Add("0123456789");
+
+            //Verificando se um valor existe na Lista
+            Console.WriteLine("listaHashSet.Contains(Carro) :" + listaHashSet.Contains("Carro") );
+            Console.WriteLine("listaHashSet.Contains(Aviao) :" + listaHashSet.Contains("Aviao"));
+
+            //Revome todos os elementos da lista que atendem o predicado
+            listaHashSet.RemoveWhere(x => x.Length == 10); //Neste caso todos os valores que o tamanho seja igual a 10
+
+            //Listando valores da Lista via foreach
+            Console.WriteLine("*** Lista HashSet ***");
+            foreach (string obj in listaHashSet)
+            {
+                Console.WriteLine(obj);
+            }
+                        
+            //Remove um Elemento em especifico informado no parãmetro
+            listaHashSet.Remove("Carro");
+
+            //Remove todos os Elementos da Lista
+            listaHashSet.Clear();
+        }
+    }
+}
---
+++ WorkingGenerics/HashSetAndSortedSet/SortedSetOp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkingGenerics.HashSetAndSortedSet
+{
+    //Classe para testar SortedSet
+    class SortedSetOp
+    {
+        //Método Main
+        static void Main(string[] args)
+        {
+            //Declaração variável SortedSet e definindo valores
+            SortedSet<int> ss01 = new SortedSet<int>() { 0,1,2,3,4,5,6,7,8,9 };
+            SortedSet<int> ss02 = new SortedSet<int>() { 5, 6, 7, 8, 9,10,11,12,13,14 };
+
+            //UNIAO
+            SortedSet<int> ss03 = new SortedSet<int>(ss01); //Declarado, Instânciado variável recebendo toda a coleção de ss01
+            ss03.UnionWith(ss02); //ss03 recebe todos valores de ss02 que não exista em ss03
+
+            //INTERSEÇÃO
+            SortedSet<int> ss04 = new SortedSet<int>(ss01);
+            ss04.IntersectWith(ss02);//ss04 recebe a interseção de valores entre ss04 e ss02
+
+            //DIFERENÇA
+            SortedSet<int> ss05 = new SortedSet<int>(ss01);
+            ss05.ExceptWith(ss02);//ss05 recebe a diferença do que tem em ss05 e não existe em ss02
+
+            //Chamando o Método para Imprimir
+            ImprimirColecao(ss01);
+            ImprimirColecao(ss02);
+            ImprimirColecao(ss03);
+            ImprimirColecao(ss04);
+            ImprimirColecao(ss05);
+
+        }
+
+        //Método para Imprimir Coleção
+        static void ImprimirColecao<T>(IEnumerable<T> colecao)
+        {
+            foreach (T obj in colecao)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
