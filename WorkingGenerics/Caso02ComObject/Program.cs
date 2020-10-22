e6dc69bec6bac7ce0f8831fcd5b2a4ef210293b0
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-22 14:13:45 -0300

    Add files via upload

---
+++ WorkingGenerics/Caso02ComObject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WorkingGenerics.Caso02ComObject
+{
+    //Classe Principal
+    class Program
+    {
+        //Método Principal
+        /*static void Main(string[] args)
+        {
+            //Declarando variável
+            ServicoImpressao simp = new ServicoImpressao();
+
+            Console.Write("Quantos valores deseja digitar? : ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)//Para controlar a quantidade que será digitada
+            {
+                Console.Write($"Valor {i} : ");
+                object x = Console.ReadLine(); //Irá aceitar Nºs e Strings
+                simp.AdicionarValores(x);
+            }
+
+            //Imprimindo Valores
+            simp.Imprimir();
+            Console.WriteLine("Primeiro Valor : " + simp.PrimeiroValor() );
+        }*/
+    }
+}
---
+++ WorkingGenerics/Caso02ComObject/ServicoImpressao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace WorkingGenerics.Caso02ComObject
+{
+    //Classe entidade contêm os serviços de impressão
+    //Seus atributos são do Tipo Object, podendo receber qualquer valor de entrada
+    class ServicoImpressao
+    {
+        //Atributos
+        private object[] _valores = new object[10];
+        private int _count = 0; //Para controlar as posições
+
+        //Método para Adicionar valores ao Vetor
+        public void AdicionarValores(object valor)
+        {
+            if (_count == 10 ) ///Lança exceção de erro caso esteja cheio
+            {
+                throw new InvalidOperationException("InvalidOperationException - Vetor está cheio!");
+            }
+            _valores[_count] = valor;
+            _count++;
+        }
+
+        //Método para retornar o primeiro elemento do Vetor
+        public object PrimeiroValor()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("InvalidOperationException - Vetor é Vazio ainda!");
+            }
+            return _valores[0];
+        }
+
+        //Método para imprimir valor
+        public void Imprimir()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count -1; i++) //Imprime da Posição 0 a penultima
+            {
+                Console.Write(_valores[i] + ", ");
+            }
+            if (_count > 0) //Imprime a Última posição com valor
+            {
+                Console.Write(_valores[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
