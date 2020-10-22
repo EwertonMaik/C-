448744d99e9f54644a21a839dabb6d8ab38e6bb8
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-22 11:14:52 -0300

    Add files via upload

---
+++ Interfaces/InterfaceIComparable/Funcionario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace WorkingInterfaces.InterfaceIComparable
+{
+    //Classe entidade Funcionário - Implementando Interface Icomparable
+    class Funcionario : IComparable
+    {
+        //Atributos autoProperties
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        //Definindo um Construtor para receber um string de funcionário e salvar em um vetor
+        public Funcionario (string csvFuncionario)
+        {
+            string[] vetor = csvFuncionario.Split(",");//Recebe a linha do arquivo csv e salva quebrado no vetor de strigs nome e salario
+            Nome = vetor[0];
+            Salario = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+        }
+
+        //Sobreposição com o Método ToString
+        public override string ToString()
+        {
+            return "Nome : " + Nome + " Salário : " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        //Método Implementado da Interface IComparable
+        //Interface IComparable - Interface para comparar um objeto com outro
+        //menor, maior ou igual - Compara dois objetos retornando um número inteiro
+        public int CompareTo(object obj)
+        {
+            if (! (obj is Funcionario) ) //Se o parâmetro do objeto recebido não for um Funcionário
+            {
+                throw new ArgumentException("Erro ArgumentException - Não é um Objeto Funcionário!");
+            }
+
+            Funcionario func = obj as Funcionario;//DownCasting, Converte um Tipo Generico(Object) para um especifico, no caso, Funcionário
+            return Nome.CompareTo(func.Nome);
+        }
+    }
+}
---
+++ Interfaces/InterfaceIComparable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WorkingInterfaces.InterfaceIComparable
+{
+    //Classe com Método Principal
+    class Program
+    {
+        //Método Main
+        static void Main(string[] args)
+        {
+            //Atributo com o caminho do arquivo que será lido e comparado
+            string caminho = @"C:\Udemy\Funcionarios.txt";
+
+            try
+            {
+                //Executando dentro de um bloco using, após seu termino é finalizado também a conexão com o arquivo
+                using (StreamReader sr = File.OpenText(caminho) )
+                {
+                    List<Funcionario> list = new List<Funcionario>(); //Cria uma lista de Funcionários que receberá os dados lidos do txt
+                    while(! sr.EndOfStream)// Enquanto não foor o fim do arquivo
+                    {
+                        list.Add(new Funcionario(sr.ReadLine() ) ); //Recebe a linha lida passa ao Contrutor de Funcionário
+                    }
+                    list.Sort(); //Ordena a lista de Funcionários usando a Interface IComparable
+
+                    //Imprimindo valores em Tela
+                    foreach (Funcionario obj in list)
+                    {
+                        Console.WriteLine(obj);
+                    }
+                }
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine("Erro IOException : " + ioe.Message);
+            }
+        }
+    }
+}
