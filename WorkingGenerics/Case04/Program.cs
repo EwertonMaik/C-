d3f32ac6875309568c989fe8f2ddd6da6881a49e
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-22 21:11:49 -0300

    Add files via upload

---
+++ WorkingGenerics/Case04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WorkingGenerics.Case04
+{
+    //Classe Principal
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarado Lista Hashset do tipo RegistroLog  que não pode ter valor repetido
+            HashSet<RegistroLog> listHS = new HashSet<RegistroLog>();
+
+            Console.Write("Digite o Caminho do arquivo que será Lido : ");
+            string caminho = Console.ReadLine();
+            //C:\Udemy\C# COMPLETO 2020 Programação Orientada a Objetos + Projetos 37_horas\CAP_15\UsuarioLog.txt
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(caminho)) //Usando Bloco using para fechar a conexão com o arquivo assim se for encerrado
+                {
+                    while (!sr.EndOfStream) //Irá executar até o fim do Arquivo
+                    {
+                        string[] line = sr.ReadLine().Split(' '); //Pega a linha lida e quebra no espaço e salvo no vetor
+                        string nome = line[0];
+                        DateTime dthr = DateTime.Parse(line[1]);
+                        listHS.Add(new RegistroLog(nome, dthr) ); //Adiciona na Lista HashSet de RegistroLog
+                    }
+                    Console.WriteLine("Total de Usuários : " + listHS.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro IOException : " +e.Message);
+            }
+        }
+    }
+}
---
+++ WorkingGenerics/Case04/RegistroLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WorkingGenerics.Case04
+{
+    //Classe Entidade - Resgitro Log - Representa os atributos de um arquivo de log externo que será lido
+    class RegistroLog
+    {
+        //Atributos AutoProperties
+        public string Nome { get; set; }
+        public DateTime DataHora { get; set; }
+
+        //Método Construtor
+        public RegistroLog(string nome, DateTime dataHora)
+        {
+            Nome = nome;
+            DataHora = dataHora;
+        }
+
+        //Implementado Método GetHashCode
+        public override int GetHashCode()
+        {
+            return Nome.GetHashCode();
+        }
+
+        //Implementado Método Equals - Permeritá que na comparação da lista HashSet, não seja adicionado valor repetido
+        public override bool Equals(object obj)
+        {
+            if (!(obj is RegistroLog)) // Retorna falso caso parâmetro recebido não seja RegistroLog
+            {
+                return false;
+            }
+            RegistroLog other = obj as RegistroLog; //DownCasting
+            return Nome.Equals(other.Nome); //Realiza a comparação
+        }
+    }
+}
