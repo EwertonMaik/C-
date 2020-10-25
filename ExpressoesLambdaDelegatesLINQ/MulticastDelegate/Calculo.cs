ce3ee96c021eacca8ca195b98e547c8fa84ad830
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-25 08:55:18 -0300

    Add files via upload

---
+++ ExpressoesLambdaDelegatesLINQ/MulticastDelegate/Calculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExpressoesLambdaDelegatesLINQ.MulticastDelegate
+{
+    //Classe Usada para Testar Multcast Delegate, ambos 2 parâmetros
+    class Calculo
+    {
+        //Método Maio
+        public static void Maior(double a, double b)
+        {
+            double maior = (a > b) ? a : b;
+            Console.WriteLine("Maior = " + maior);
+        }
+
+        //Método Soma
+        public static void Soma(double a, double b)
+        {
+            double soma = a + b;
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
---
+++ ExpressoesLambdaDelegatesLINQ/MulticastDelegate/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExpressoesLambdaDelegatesLINQ.MulticastDelegate
+{
+    //Classe Principal
+    class Program
+    {
+        //Declarando Delegate - Como void
+        delegate void OperacaoNumerica(double n1, double n2);
+
+        //Método Main
+        static void Main(string[] args)
+        {
+            //Declaração Atributos
+            double a = 10;
+            double b = 20;
+
+            //Instânciando Delegate - Recebendo referência de mais de 1 método
+            OperacaoNumerica ope = Calculo.Maior;
+            ope += Calculo.Soma;
+
+            //Os 2 métodos seram executados
+            ope(a, b);
+            //ou
+            ope.Invoke(a, b);
+        }
+    }
+}
